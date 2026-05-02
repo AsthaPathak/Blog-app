8487804d95d68439905e34ae6c70fbc7e44d3c3a
AsthaPathak <asthapathak106@gmail.com>
2026-05-02 21:09:54 +0530

    Added dynamic port for deployment

--- server.js
+++ server.js
@@ -92,4 +92,6 @@ app.post('/api/comments', (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
